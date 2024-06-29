c45321e59b3c09633a77341f36700be0f26d2e28
mr_noppo <takizawa20383461@gmail.com>
2024-06-29 12:11:00 +0900

    ボタンの作成と配置

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,10 @@
+import Input from "@/components/input";
+import { useState } from "react";
+
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -11,7 +17,36 @@ const Auth = () => {
                             Sign In
                         </h2>
                         <div className="flex flex-col gap-4">
+                            <Input 
+                                label="Username"
+                                onChange={(ev: any) => {setName(ev.target.value)}}
+                                id="name"
+                                value={name}
+                            />
+                            <Input 
+                                label="Email"
+                                onChange={(ev: any) => {setEmail(ev.target.value)}}
+                                id="email"
+                                type="email"
+                                value={email}
+                            />
+                            <Input 
+                                label="Password"
+                                onChange={(ev: any) => {setPassword(ev.target.value)}}
+                                id="password"
+                                type="password"
+                                value={password}
+                            />
                         </div>
+                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            Login
+                        </button>
+                        <p className="text-neutral-500 mt-12">
+                            First time using Netflix?
+                            <span className="text-white ml-1 hover:underline cursor-pointer">
+                                Create and account
+                            </span>
+                        </p>
                     </div>
                 </div>
             </div>
