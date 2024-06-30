c7e564b5ed3a88ec0f412beac3d9c07d76ca8527
mr_noppo <takizawa20383461@gmail.com>
2024-06-30 22:40:29 +0900

    ユーザー作成、authソース作成

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,5 @@
 import Input from "@/components/input";
+import axios from "axios";
 import { useCallback, useState } from "react";
 
 const Auth = () => {
@@ -11,6 +12,19 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant)=> currentVariant === "login" ? "register" : "login")
     },[])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post("/api/register", {
+                email,
+                name,
+                password
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    },[email, name, password])
+
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -46,7 +60,7 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
